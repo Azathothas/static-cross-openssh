d84e9330f9d9b19341cabc18cc7fd62f1195297d
Ajam <AjamX101@gmail.com>
2023-08-11 09:48:36 +0545

    Update zlib.mk

--- package/zlib/zlib.mk
+++ package/zlib/zlib.mk
@@ -1,5 +1,7 @@
-zlib/VERSION := 1.2.13
-zlib/TARBALL := https://zlib.net/zlib-$(zlib/VERSION).tar.gz
+zlib/VERSION := $(shell curl -qfsSL https://api.github.com/repos/madler/zlib/releases | jq -r '.[0].name' | sed 's/[^0-9.]//g')
+zlib/TARBALL := https://zlib.net/current/zlib.tar.gz
+# zlib/TARBALL := https://zlib.net/zlib-$(zlib/VERSION).tar.gz
+# zlib/TARBALL := https://github.com/madler/zlib/releases/download/v$(zlib/VERSION)/zlib-$(zlib/VERSION).tar.gz
 
 zlib/dir = $(build_dir)/zlib/zlib-$(zlib/VERSION)
 
