596b27321de0c2401e64b2b305574418a00e4974
Ajam <AjamX101@gmail.com>
2023-08-11 10:36:16 +0545

    Update openssl.mk

--- package/openssl/openssl.mk
+++ package/openssl/openssl.mk
@@ -1,5 +1,8 @@
+# Use Curl + jq + GitHub API to always fetch @latest Version
 openssl/VERSION := $(shell curl -qfsSL https://api.github.com/repos/openssl/openssl/releases | jq -r '.[0].name' | sed 's/[^0-9.]//g')
+#openssl/VERSION := 1.1.1u
 openssl/TARBALL := https://www.openssl.org/source/openssl-$(openssl/VERSION).tar.gz
+# We can also use these alt channels: 
 # openssl/TARBALL := https://github.com/openssl/openssl/releases/download/openssl-$(openssl/VERSION)/openssl-$(openssl/VERSION).tar.gz
 # openssl/TARBALL := $( shell curl -qfsSL https://api.github.com/repos/openssl/openssl/releases/latest | jq -r '.tarball_url')
 
